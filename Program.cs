e5258cc78af0d369922bd77ed056c9a3397ae046
Jure Pustoslemšek <jure.poglednik@gmail.com>
2024-01-22 17:31:07 +0100

    log config

--- Program.cs
+++ Program.cs
@@ -94,7 +94,7 @@ public class Program
     {
         builder.Host.UseSerilog((context, config) => {
             config.ReadFrom.Configuration(builder.Configuration)
-                // .Enrich,WithCorrelationId()
+                .Enrich.WithCorrelationId()
                 .Enrich.WithCorrelationIdHeader("X-Correlation-Id");
         });
     }
